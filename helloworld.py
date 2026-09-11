39abe66a53235311f25503e0706f8ac0e019a7d8
GraceDaw <swaroopgunika@gmail.com>
2026-09-11 22:28:05 +0530

    hehehe

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
 
-
+a= (input(""))
 b = (input("enter 2nd no: "))
 print(b)
-sum = int(a) + int(b)
+sum = 8
 print(sum)
+# hw : 2 table , avg nikalna , 3 table , counting , prime numbers print karwao
+print("u nub")
